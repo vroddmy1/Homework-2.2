dbe4bcd7181b1a0570e0510e60bc5a269596ead1
vroddmy <35878859+vroddmy@users.noreply.github.com>
2018-02-17 14:53:27 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,35 +1,32 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ru">
 <head>
-  <meta charset="UTF-8">
-  <title>Загрузка нового теста</title>
+    <meta charset="UTF-8">
+    <title>Загрузка </title>
 </head>
 <body>
-<article>
-  <nav>
-    <ul>
-      <li><a href="list.php">Список тестов</a></li>
-    </ul>   
-  </nav>
-<section>
-<h1>Загрузка нового теста</h1>
-  <form method="post" action="" enctype="multipart/form-data">
-    Загрузить новый тест .JSON<br><br>
-    <input type="file" name="myfile"><br><br>
-    <input type="submit" value="ОТПРАВИТЬ">
-  </form>
-</section>
-</article>
+
+<form action="admin.php" enctype="multipart/form-data" method="post">   
+    <h3>Загрузите файл .json </h3>
+    <input type="file" name="testFile" accept=".json"><br><br>
+    <input type="submit" value="Загрузить" name="submit">
+</form>
+<br><h3><a href="list.php">Список  тестов</a></h3>
+
+<?php
+if (isset($_FILES['testFile'])) { 
+    if (is_uploaded_file($_FILES['testFile']['tmp_name'])) {     
+        $uploaddir = 'tests/';
+        $uploadfile = $uploaddir . basename($_FILES['testFile']['name']);      
+            
+            if ($_FILES['testFile']['error'] === UPLOAD_ERR_OK && move_uploaded_file($_FILES['testFile']['tmp_name'], $uploadfile)) {
+                echo "<h3>Файл загружен</h3>";
+            } else {
+                echo "<h3>Не удалось загрузить файл</h3>";
+                exit;
+            }
+    }
+}
+?>
 </body>
 </html>
-<?php 
-if (isset($_FILES['myfile'])) {
-    $file = $_FILES['myfile'];
-}
-if (!empty($file['name']) && $file['error'] == UPLOAD_ERR_OK && 
-    move_uploaded_file($file['tmp_name'], __DIR__ . '/uploadedFilesinfo.json')) {
-        echo 'Файл загружен';
-} else {
-    echo 'Файл не загружен, попробуйте еще раз.';
-}
- ?>
