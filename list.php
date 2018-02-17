c967df021f76b60453e622f3df4286163f3c470b
vroddmy <35878859+vroddmy@users.noreply.github.com>
2018-02-17 14:54:24 +0300

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,29 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ru">
 <head>
-  <meta charset="UTF-8">
-  <title>Все тесты для задания</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <title>Список  тестов</title>
 </head>
 <body>
-<article>
-  <nav>
-    <ul>
-      <li><a href="list.php">Список тестов</a></li>
-      <li><a href="admin.php">Добавить тест</a></li>
-    </ul>   
-  </nav>
-<article>
-<section>
-  <h1>Все тесты</h1>
-  <ul>
-<li><a href='test.php?num=1'>Тест 1</a></li>
-<li><a href='test.php?num=2'>Тест 2</a></li>
-<li><a href='test.php?num=3'>Тест 3</a></li>
-<li><a href='test.php?num=4'>Тест 4</a></li>
-<li><a href='test.php?num=5'>Тест 5</a></li>
-  </ul>
+    <h1>Список  тестов</h1>
+    <table>
 
-</section>
-</article>
+<?php
+    $files = array_slice(scandir('tests/' ), 3);
+    $count = 1;
+if (!empty($files)) {
+    foreach ($files as $file) {
+        if (end(explode('.', $file)) === 'json') {
+            $test = pathinfo($file)['filename'];
+            echo '<tr><td>' . $count . ") " . '</td><td><a href="test.php?name=' . $test . '">' . $test . '</a></td></tr>';
+            $count++;
+        }
+    }
+} else {
+    echo '<tr><td>Нет тестов</td></tr>';
+
+}
+?>
+    </table>
 </body>
 </html>
